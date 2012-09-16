f1fda416da6436c0f83b0234f3e39f187a66e684
Jyrki Muukkonen <jyrki.muukkonen@f-secure.com>
2012-09-17 02:57:57 +0300

    initial parser done, and figured out most of the sample elements

---
+++ pyambit/parser.py
@@ -0,0 +1,104 @@
+'''
+Created on Sep 16, 2012
+
+@author: jvtm
+
+Plan:
+ - generic parser function
+ - different formatters: GPX, Pythonic values, ...
+     (it would be silly to convert some text values to python and then back...)
+ - optionally merging different sample blocks
+ - pausing? get example data
+ - waypoints / lap times?
+'''
+
+from cStringIO import StringIO
+from datetime import timedelta
+import sys
+import dateutil.parser
+import math
+
+try:
+    import lxml.etree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+
+def parse_utc(text):
+    """ with or without millisecs """
+    return dateutil.parser.parse(text)
+
+
+def parse_time(text):
+    """ seconds since start """
+    return timedelta(seconds=float(text))
+
+
+def parse_hr(text):
+    """ heart rate seems to be beats-per-second, convert to bpm """
+    return int(60 * float(text))
+
+
+def parse_rad(text):
+    """ coordinates from rad -> deg """
+    return math.degrees(float(text))
+
+
+def parse_temperature(text):
+    """ temperature from kelvin -> celsius """
+    celsius = float(text) - 273.15
+    return round(celsius, 1)
+
+# XXX: if outputting strings anyway, there's no need to parse floats...
+PARSERS = {
+    # on both sample entry types
+    'Time': parse_time,     # time from start. need to check how pausing affects this...
+    'UTC': parse_utc,       # it actually would work as GPX <time> directly
+
+    # GPS sample entry
+    'Latitude': parse_rad,
+    'Longitude': parse_rad,
+    'GPSAltitude': int,     # TODO: compare against baro
+    'GPSHeading': float,    # convert?
+    'EHPE': int,   # TODO: find out what igotu2gpx outputs
+
+    # Suunto sample entry
+    'VerticalSpeed': float,             # m/s
+    'HR': parse_hr,
+    'EnergyConsumption': float,         # float, but what...
+    'Temperature': parse_temperature,   # kelvins? really? :)
+    'SeaLevelPressure': int,            # ???
+    'Altitude': int,                    # XXX: from baro?
+    'Distance': int,
+    'Speed': float,                     # m/s
+}
+
+
+def fix_ambit_data(file_obj):
+    data = file_obj.readline()
+    data += "<root>"
+    data += file_obj.read()
+    data += "</root>\n"
+    return StringIO(data)
+
+
+def parse_ambit_samples(file_obj):
+    # ambit data is not valid xml. need to add a fake top level entry.
+    tree = ET.parse(file_obj)
+    item = {}
+    for sample in tree.find('samples'):
+        is_gps_sample = (sample.find('Latitude') is not None)
+        for child in sample:
+            parser = PARSERS.get(child.tag)
+            if not callable(parser):
+                continue
+            item[child.tag] = parser(child.text)
+        if is_gps_sample:
+            # merging data. gps samples have full seconds, so it's kinda nicer...
+            yield item
+            item = {}
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'rb') as fob:
+        for item in parse_ambit_samples(fix_ambit_data(fob)):
+            print item
